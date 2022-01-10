2634ebde9459975dd8b382ae0368497bc64b9a75
marinasvistunova <54927872+polinalazebnikova@users.noreply.github.com>
2022-01-10 19:45:00 +0300

    first-commit

---
+++ webpack/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = = {
+  entry: {
+    app: "./js/main.js",
+    styles: "./scss/main.scss",
+  },
+  output: {
+    path: path.join(path.dirname(__dirname), "assets", "static", "gen"),
+    filename: "[name].js",
+  },
+  devtool: "source-map",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(woff2?|ttf|eot|svg|png|jpe?g|gif)$/,
+        use: ["file"],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+};
